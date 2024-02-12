d3c03838c725e9e42046dadcf5cd085efe5402a1
iAmSherm2 <trace.anderson@students.fhu.edu>
2024-02-11 19:54:33 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 #REMOVE PASS AND FIX THIS FUNCTION
 def palindrome(word):
-    pass
+    word = word.lower().replace(" ", "")
+    if not word:
+        return False
+    left, right = 0, len(word) - 1
+    while left < right:
+        if word[left] != word[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
+
 
 if __name__ == '__main__': 
     #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+   user = str(input())
+   result = palindrome(user)
+   print(result)
